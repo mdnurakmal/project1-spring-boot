97ee3fadaa4c9447ee2e8a7684b5a7e2f3d936ea
Nur Akmal <mdnurakmal@gmail.com>
2021-10-16 01:20:16 +0800

    Update KafkaConstants.java

--- src/main/java/com/mdnurakmal/chat/constants/KafkaConstants.java
+++ src/main/java/com/mdnurakmal/chat/constants/KafkaConstants.java
@@ -2,7 +2,7 @@ package com.mdnurakmal.chat.constants;
 
 
 public class KafkaConstants {
-    public static final String KAFKA_TOPIC = "kafka-chat-3";
+    public static final String KAFKA_TOPIC = "mytopic";
     public static final String GROUP_ID = "kafka-sandbox";
-    public static final String KAFKA_BROKER = "localhost:9092";
+    public static final String KAFKA_BROKER = "kafka.default.svc.cluster.local:9092";
 }

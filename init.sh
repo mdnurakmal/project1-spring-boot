d5841e27d8465a002aa1cb98727df21d6c04fdbf
Nur Akmal <mdnurakmal@gmail.com>
2021-10-16 14:08:53 +0800

    update

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sed -i "s/clusterip/$1/" $2
+clusterip=$(kubectl get services demo -o=jsonpath='{.spec.clusterIP}')
+
+sed -i "s/clusterip/$clusterip/" configmap.yaml
+kubectl apply -f configmap.yaml

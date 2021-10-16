ce78a6819f6b43ffb6dd4a552b1c7b8db32f85a5
Nur Akmal <mdnurakmal@gmail.com>
2021-10-16 14:35:51 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@
 set -x #echo onnano
 
 echo "hello"
-temp=$(kubectl get services demo -o=jsonpath='{.spec.clusterIP}')
+temp=$(kubectl get services kafka -o=jsonpath='{.spec.clusterIP}')
 echo "running" $temp
 sed -i "s/clusterip/$temp/" ./configmap.yaml
 kubectl apply -f ./configmap.yaml
+kubectl apply -f ./deployment.yaml

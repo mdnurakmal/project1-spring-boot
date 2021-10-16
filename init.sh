d83cb705915e0de5225a89d88375c4c7df7f0e61
Nur Akmal <mdnurakmal@gmail.com>
2021-10-16 14:12:06 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-clusterip=$(kubectl get services demo -o=jsonpath='{.spec.clusterIP}')
+clusterip=`kubectl get services demo -o=jsonpath='{.spec.clusterIP}'`
 
 sed -i "s/clusterip/$clusterip/" configmap.yaml
 kubectl apply -f configmap.yaml

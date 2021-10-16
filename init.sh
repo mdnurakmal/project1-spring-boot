b428aa6090ee385404affab5840b836d6bffe762
Nur Akmal <mdnurakmal@gmail.com>
2021-10-16 15:33:40 +0800

    update

--- init.sh
+++ init.sh
@@ -6,7 +6,8 @@ temp=$(kubectl get services kafka -o=jsonpath='{.spec.clusterIP}')
 echo "running" $temp
 sed -i "s/clusterip/$temp/" ./configmap.yaml
 
-SPRING_CONFIG_LOCATION=`echo $PWD"/configmap.yaml"`
+#SPRING_CONFIG_LOCATION=`echo $PWD"/configmap.yaml"`
+
 kubectl apply -f ./configmap.yaml
 kubectl apply -f ./deployment.yaml
 kubectl rollout restart deployment/demo

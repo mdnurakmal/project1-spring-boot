2d717359ee8168bf01bb516a98ba04671a879c56
Nur Akmal <mdnurakmal@gmail.com>
2021-10-17 14:04:18 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x #echo onnano
 
-echo "hello"
-temp=$(kubectl get services kafka -o=jsonpath='{.spec.clusterIP}')
-echo "running" $temp
-sed -i "s/clusterip/$temp/" ./configmap.yaml
+#echo "hello"
+#temp=$(kubectl get services kafka -o=jsonpath='{.spec.clusterIP}')
+#echo "running" $temp
+#sed -i "s/clusterip/$temp/" ./configmap.yaml
 
 #SPRING_CONFIG_LOCATION=`echo $PWD"/configmap.yaml"`
 

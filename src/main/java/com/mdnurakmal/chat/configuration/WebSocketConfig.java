59148dac68c81a610092709ac94e975d91a0547f
Nur Akmal <mdnurakmal@gmail.com>
2021-10-21 02:00:46 +0800

    Update WebSocketConfig.java

--- src/main/java/com/mdnurakmal/chat/configuration/WebSocketConfig.java
+++ src/main/java/com/mdnurakmal/chat/configuration/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.mdnurakmal.chat.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -9,15 +10,35 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // chat client will use this to connect to the server
-        registry.addEndpoint("/").setAllowedOrigins("http://localhost:4200","https://mdnurakmal.com","https://wschat.mdnurakmal.com").withSockJS().setHeartbeatTime(20000)
-                .setDisconnectDelay(10000);
+        registry.addEndpoint("/").setAllowedOrigins("http://localhost:4200","https://mdnurakmal.com","https://wschat.mdnurakmal.com").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        ThreadPoolTaskScheduler te = new ThreadPoolTaskScheduler();
+        te.setPoolSize(1);
+        te.setThreadNamePrefix("wss-heartbeat-thread-");
+        te.initialize();
+
+        registry.enableSimpleBroker("/")
+                /**
+                 * Configure the value for the heartbeat settings. The first number
+                 * represents how often the server will write or send a heartbeat.
+                 * The second is how often the client should write. 0 means no heartbeats.
+                 * <p>By default this is set to "0, 0" unless the {@link #setTaskScheduler
+                 * taskScheduler} in which case the default becomes "10000,10000"
+                 * (in milliseconds).
+                 * @since 4.2
+                 */
+                .setHeartbeatValue(new long[]{5000, 5000})
+                .setTaskScheduler(te);
+
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic/");
     }

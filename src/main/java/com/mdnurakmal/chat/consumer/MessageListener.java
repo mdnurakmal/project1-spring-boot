6a3d665b828f2b59564f56544a1bd25334b79627
Nur Akmal <mdnurakmal@gmail.com>
2021-10-21 22:33:21 +0800

    Update MessageListener.java

--- src/main/java/com/mdnurakmal/chat/consumer/MessageListener.java
+++ src/main/java/com/mdnurakmal/chat/consumer/MessageListener.java
@@ -1,9 +1,13 @@
 package com.mdnurakmal.chat.consumer;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mdnurakmal.chat.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
@@ -15,8 +19,10 @@ public class MessageListener {
     SimpMessagingTemplate template;
 
     @KafkaListener(topics = TOPIC)
-    public void listen(Message message) {
+    public void listen(@Payload String message) throws JsonProcessingException {
+        JsonNode data = new ObjectMapper().readTree(message);
         System.out.println("sending via kafka listener..");
         System.out.println("Message received: " + message.toString());
+        System.out.println("Json converted: " + data);
     }
 }
